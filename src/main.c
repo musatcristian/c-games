4a08727a470b3406a54e95124fcc90514704f357
Cristian Musat <cristutz00@gmail.com>
2025-02-10 15:24:48 +0200

    2 paddles

--- src/main.c
+++ src/main.c
@@ -20,8 +20,11 @@ typedef enum
 int main(void)
 {
     POSITION pos = UP;
-    int posX = 0;
-    int posY = height - p_height / 2;
+    int l_posX = 0;
+    int l_posY = height / 2 - p_height / 2;
+
+    int r_posX = width - p_width;
+    int r_posY = height / 2 - p_height / 2;
 
     // raylib stuff
     Vector2 posBall = {
@@ -32,37 +35,60 @@ int main(void)
         .x = 5.0f,
         .y = 4.0f,
     };
-    uint8_t collision = 0;
+    uint8_t l_collision = 0;
+    uint8_t r_collision = 0;
 
     SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(width, height, "Raylib App");
     SetTargetFPS(fps);
-    Rectangle paddle =
-        {.x = posX, .y = posY, .height = p_height, .width = p_width};
+    Rectangle l_paddle = {
+        .x = l_posX,
+        .y = l_posY,
+        .height = p_height,
+        .width = p_width,
+    };
+
+    Rectangle r_paddle = {
+        .x = r_posX,
+        .y = r_posY,
+        .height = p_height,
+        .width = p_width,
+    };
 
     while (!WindowShouldClose())
     {
 
-        if (IsKeyDown(KEY_W) && paddle.y > 0)
+        if (IsKeyDown(KEY_W) && l_paddle.y > 0)
         {
-            paddle.y -= step;
+            l_paddle.y -= step;
         }
 
-        if (IsKeyDown(KEY_S) && paddle.y < height - p_height)
+        if (IsKeyDown(KEY_S) && l_paddle.y < height - p_height)
         {
-            paddle.y += step;
+            l_paddle.y += step;
+        }
+
+        if (IsKeyDown(KEY_UP) && r_paddle.y > 0)
+        {
+            r_paddle.y -= step;
+        }
+
+        if (IsKeyDown(KEY_DOWN) && r_paddle.y < height - p_height)
+        {
+            r_paddle.y += step;
         }
         posBall.x += velBall.x;
         posBall.y += velBall.y;
 
-        collision = CheckCollisionCircleRec(posBall, (float)radius, paddle);
+        l_collision = CheckCollisionCircleRec(posBall, (float)radius, l_paddle);
+        r_collision = CheckCollisionCircleRec(posBall, (float)radius, r_paddle);
         // Check walls collision for bouncing
         if ((posBall.x >= (GetScreenWidth() - radius)) || (posBall.x <= radius))
             velBall.x *= -1.0f;
         if ((posBall.y >= (GetScreenHeight() - radius)) || (posBall.y <= radius))
             velBall.y *= -1.0f;
 
-        if (collision)
+        if (r_collision || l_collision)
         {
             velBall.x *= -1.0f;
         }
@@ -70,8 +96,9 @@ int main(void)
         BeginDrawing();
         ClearBackground(DARKPURPLE);
         DrawText("Hello, Bibi!", 250, 100, 50, RAYWHITE);
-        DrawRectangleRec(paddle, collision ? RED : ORANGE);
-        DrawCircleV(posBall, (float)radius, BLUE);
+        DrawRectangleRec(l_paddle, l_collision ? RED : ORANGE);
+        DrawRectangleRec(r_paddle, r_collision ? RED : ORANGE);
+        DrawCircleV(posBall, (float)radius, r_collision || l_collision ? BLUE : DARKBLUE);
         EndDrawing();
     }
 
