13faed89a54131aa4c454cb4e0024c6fa38390a9
Cristian Musat <cristutz00@gmail.com>
2025-02-10 19:14:27 +0200

    score and start

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 default:
-	gcc src/*.c -ggdb -Iinclude -Llib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -DGRAPHICS_API_OPENGL_33 -o pong
+	gcc src/*.c -ggdb -Wall -Iinclude -Llib -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o pong
 
 
 #gcc -o exe src/main.c -I../raylib/src -L../raylib/src -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
--- src/main.c
+++ src/main.c
@@ -11,20 +11,22 @@ const uint8_t p_height = 80;
 const uint8_t p_width = 20;
 const uint8_t radius = 20;
 
-typedef enum
-{
-    UP,
-    DOWN
-} POSITION;
+// typedef enum
+// {
+//     UP,
+//     DOWN
+// } POSITION;
 
 int main(void)
 {
-    POSITION pos = UP;
+    // POSITION pos = UP;
     int l_posX = 0;
     int l_posY = height / 2 - p_height / 2;
+    uint8_t l_score = 0;
 
     int r_posX = width - p_width;
     int r_posY = height / 2 - p_height / 2;
+    uint8_t r_score = 0;
 
     // raylib stuff
     Vector2 posBall = {
@@ -55,6 +57,13 @@ int main(void)
         .width = p_width,
     };
 
+    Rectangle start_btn = {
+        .height = 50,
+        .width = 145,
+        .x = width / 2 - 85,
+        .y = 100,
+    };
+
     while (!WindowShouldClose())
     {
 
@@ -83,7 +92,7 @@ int main(void)
         l_collision = CheckCollisionCircleRec(posBall, (float)radius, l_paddle);
         r_collision = CheckCollisionCircleRec(posBall, (float)radius, r_paddle);
         // Check walls collision for bouncing
-        if ((posBall.x >= (GetScreenWidth() - radius)) || (posBall.x <= radius))
+        if ((posBall.x >= (GetScreenWidth() - radius)))
             velBall.x *= -1.0f;
         if ((posBall.y >= (GetScreenHeight() - radius)) || (posBall.y <= radius))
             velBall.y *= -1.0f;
@@ -93,12 +102,42 @@ int main(void)
             velBall.x *= -1.0f;
         }
 
+        if (posBall.x <= radius)
+        {
+            r_score += 1;
+            posBall.x = width / 2 - p_width / 2;
+            posBall.y = height / 2 - p_height / 2;
+
+            velBall.x = 0;
+            velBall.y = 0;
+        }
+
+        if (posBall.x >= width - radius)
+        {
+            l_score += 1;
+            posBall.x = width / 2 - p_width / 2;
+            posBall.y = height / 2 - p_height / 2;
+
+            velBall.x = 0;
+            velBall.y = 0;
+        }
+        Vector2 mouse_pos = GetMousePosition();
+        if (CheckCollisionPointRec(mouse_pos, start_btn) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            velBall.x = 2.0f;
+            velBall.y = -4.0f;
+        }
+
         BeginDrawing();
         ClearBackground(DARKPURPLE);
-        DrawText("Hello, Bibi!", 250, 100, 50, RAYWHITE);
+        DrawRectangleRounded(start_btn, 4.0f, 0.0, Fade(RED, 0.3f));
+        DrawText("START", width / 2 - 55, 115, 25, RAYWHITE);
+        DrawText(TextFormat("Player 1: %u", l_score), p_width + 20, 25, 20, RAYWHITE);
+        DrawText(TextFormat("Player 2: %u", r_score), width - 150, 25, 20, RAYWHITE);
+
         DrawRectangleRec(l_paddle, l_collision ? RED : ORANGE);
         DrawRectangleRec(r_paddle, r_collision ? RED : ORANGE);
-        DrawCircleV(posBall, (float)radius, r_collision || l_collision ? BLUE : DARKBLUE);
+        DrawCircleV(posBall, (float)radius, (r_collision || l_collision) ? BLUE : DARKBLUE);
         EndDrawing();
     }
 
